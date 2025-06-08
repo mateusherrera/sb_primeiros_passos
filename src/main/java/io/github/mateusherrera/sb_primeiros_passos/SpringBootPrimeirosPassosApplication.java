b0d1a23d5cf9e1ab913cf07851f16a6c14406c1d
Mateus Herrera <44376582+mateusherrera@users.noreply.github.com>
2025-06-08 18:47:31 -0300

    DOCS: Adicionando docstrings iniciais ao projeto Spring Boot

--- src/main/java/io/github/mateusherrera/sb_primeiros_passos/SpringBootPrimeirosPassosApplication.java
+++ src/main/java/io/github/mateusherrera/sb_primeiros_passos/SpringBootPrimeirosPassosApplication.java
@@ -3,9 +3,19 @@ package io.github.mateusherrera.sb_primeiros_passos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point para a aplicação Spring Boot.
+ * Primeiros passos do curso: Spring Boot Expert: JPA, REST, JWT, OAuth2 com Docker e AWS.
+ *
+ * @author Mateus Herrera
+ * @since 2025-06-08
+ */
 @SpringBootApplication
 public class SpringBootPrimeirosPassosApplication {
 
+	/**
+	 * Método principal que inicia a aplicação Spring Boot.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrimeirosPassosApplication.class, args);
 	}
