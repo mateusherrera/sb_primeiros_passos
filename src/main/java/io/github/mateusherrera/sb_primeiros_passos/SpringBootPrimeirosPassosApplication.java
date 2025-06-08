d4fb975186b23d5dbc7b9b91f0bf34731ecda283
Mateus Herrera <44376582+mateusherrera@users.noreply.github.com>
2025-06-08 17:40:56 -0300

    Initial commit

---
+++ src/main/java/io/github/mateusherrera/sb_primeiros_passos/SpringBootPrimeirosPassosApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusherrera.sb_primeiros_passos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrimeirosPassosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusherrera/sb_primeiros_passos/SpringBootPrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusherrera.sb_primeiros_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
